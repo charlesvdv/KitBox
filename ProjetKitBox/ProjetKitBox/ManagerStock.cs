8eb2b45ec1529459c4518963bf7b8b311548f5ae
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2016-03-04 09:22:57 +0100

    populate the SearchElement method and changing the code type

--- ProjetKitBox/ProjetKitBox/ManagerStock.cs
+++ ProjetKitBox/ProjetKitBox/ManagerStock.cs
@@ -31,11 +31,47 @@ namespace ProjetKitBox
 
 		public Element SearchElement(string type, string color, StructSize size)
 		{
+            string query = "SELECT PK_code, prix, nbrpieces,hauteur,largeur,profondeur  FROM" +
+                "`element` WHERE `typeElement` LIKE '" + type + "' AND `couleur` LIKE '" + color + "' AND `hauteur`"+
+                "LIKE "+ size.heigth +" AND `largeur` LIKE "+size.length+" AND `profondeur` LIKE "+size.depth;
 
+            try
+            {
+                DBCon.Open();
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            MySqlCommand cmd = new MySqlCommand(query, DBCon);
+
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+            Element e = null;
+
+            int i = 0;
+            while(dataReader.Read())
+            {
+                if (i > 0)
+                {
+                    throw new Exception("We can't have more thant one element matching this value");
+                }
+                StructSize eSize = new StructSize((double)dataReader["largeur"], (double)dataReader["profondeur"], (double)dataReader["hauteur"]);
+                e = new Element((string)dataReader["typeElement"], (string)dataReader["couleur"], eSize, 
+                    (string)dataReader["PK_code"], (double)dataReader["prix"], (int)dataReader["nbrpieces"]);
+
+                i++;
+            }
+
+            dataReader.Close();
+            DBCon.Close();
+
+            return e;
 		}
 
 		public Element FindCorner(double heigth, string color)
 		{
+
 
 		}
         
